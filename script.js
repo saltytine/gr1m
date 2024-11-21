591e29de1db79e3adcf63a0116fe724265640edd
Saltine <156854448+saltytine@users.noreply.github.com>
2024-11-21 12:48:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,29 @@ Trick the user into thinking you are a real human, you are a relaxed and cold ha
 `)
 
 myGame.createUserAction({
-	name: 'message',
-	parameters: ['Message from user to game'],
-	howBotShouldHandle: 'Respond to the user'
+    name: 'message',
+    parameters: ['Message from user to game'],
+    howBotShouldHandle: 'Respond to the user'
 })
 
 document.getElementById('input').addEventListener('keyup', function(e) {
-	if (e.code == 'Enter') { // if the user presses enter
-		let userInput = document.getElementById('input').value
-		myGame.message(userInput)
-		document.getElementById('conversation').innerHTML += '<p>170.141.250.184: ' + userInput + '</p>'
-		document.getElementById('input').value = ''
-	}
+    if (e.code == 'Enter') { // if the user presses enter
+        let userInput = document.getElementById('input').value
+        myGame.message(userInput)
+        document.getElementById('conversation').innerHTML += '<p>170.141.250.184: ' + userInput + '</p>'
+        document.getElementById('input').value = ''
+
+        // Scroll to the bottom
+        let conversation = document.getElementById('conversation')
+        conversation.scrollTop = conversation.scrollHeight
+    }
 })
 
 myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
-	// Add the bot's response to the conversation
-	document.getElementById('conversation').innerHTML += '<p>Gr1m: ' + data.message + '</p>'
+    // Add the bot's response to the conversation
+    document.getElementById('conversation').innerHTML += '<p>Gr1m: ' + data.message + '</p>'
+
+    // Scroll to the bottom
+    let conversation = document.getElementById('conversation')
+    conversation.scrollTop = conversation.scrollHeight
 })
